16066f20a999553f3314594f4657c00b874bd5a8
shrikale32 <shrikale32@gmail.com>
2021-12-20 23:42:11 -0500

    empty

--- src/components/login.jsx
+++ src/components/login.jsx
@@ -1,11 +1,21 @@
 import React from 'react';
+import { Form } from "react-bootstrap";
 
 function login(props) {
+    const style1={
+        marginLeft: 220,
+        marginRight: 220
+    };
+    
     return (
         <div>
-            <h1>login</h1>
+            <img style={style1} src='banner.png' width="800" height="10"></img>
         </div>
+        
+        
     );
 }
+
+
 
 export default login;
